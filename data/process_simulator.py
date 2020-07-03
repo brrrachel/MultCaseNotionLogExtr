cc969cd988c69fad892ea19aee1b2ffeab4a7baa
Rachel <rachel.brabender@student.hpi.de>
2020-07-03 22:47:19 +0200

    changed table columns and regenerated xoc log and new config file

--- data/process_simulator.py
+++ data/process_simulator.py
@@ -16,22 +16,21 @@ create_rental -> confirm_payment -> confirm_rental -> lend_inventory -> return_i
 class ProcessSimulator:
 
     def __init__(self):
-        path = 'availableData/'
-        print(pd.__version__)
+        self.path = 'generatedData/'
 
         # load available data
-        self.address = pd.read_csv(path + 'address.csv', index_col=0)
-        self.brand = pd.read_csv(path + 'brand.csv', index_col=0)
-        self.customer = pd.read_csv(path + 'customer.csv', index_col=0)
-        self.equipment = pd.read_csv(path + 'equipment.csv', index_col=0)
-        self.inventory = pd.read_csv(path + 'inventory.csv', index_col=0)
-        self.staff = pd.read_csv(path + 'staff.csv', index_col=0)
-        self.store = pd.read_csv(path + 'store.csv', index_col=0)
+        self.address = pd.read_csv(self.path + 'address.csv', index_col=0)
+        self.brand = pd.read_csv(self.path + 'brand.csv', index_col=0)
+        self.customer = pd.read_csv(self.path + 'customer.csv', index_col=0)
+        self.equipment = pd.read_csv(self.path + 'equipment.csv', index_col=0)
+        self.inventory = pd.read_csv(self.path + 'inventory.csv', index_col=0)
+        self.staff = pd.read_csv(self.path + 'staff.csv', index_col=0)
+        self.store = pd.read_csv(self.path + 'store.csv', index_col=0)
 
         # create additional tables
         self.rental_orders = pd.DataFrame(columns=['customer_id', 'created_date', 'confirmed_date'])
-        self.lended_inventory = pd.DataFrame(columns=['rental_id', 'inventory_id', 'created_date', 'cancel_date', 'lend_date', 'return_date', 'inspection_id'])
-        self.inspections = pd.DataFrame(columns=['inspector_id', 'inspection_date', 'payment_id']) # inspector == staff
+        self.lended_inventory = pd.DataFrame(columns=['rental_id', 'inventory_id', 'created_date', 'cancel_date', 'lend_date', 'return_date'])
+        self.inspections = pd.DataFrame(columns=['inspector_id', 'inspection_date', 'lended_inventory_id']) # inspector == staff
         self.payments = pd.DataFrame(columns=['rental_id', 'value', 'created_date', 'confirmed_date'])
 
     '''
@@ -51,20 +50,14 @@ class ProcessSimulator:
         equipment_id = self.inventory.iloc[inventory_id-1]['equipment_id']
         return self.equipment.iloc[equipment_id-1]['rental_rate'].item()
 
-    def __get_overall_lended_inventory_ids__(self) -> list:
-        return list(self.lended_inventory[self.lended_inventory['inspection_id'].isna() & self.lended_inventory['cancel_date'].isna()]['inventory_id'])
-        
     def __get_staff_id_for_inspection__(self, inventory_id: int) -> int:
         store_id = self.inventory.iloc[inventory_id]['store_id']
         staff_ids = self.staff.loc[(self.staff['store_id'] == store_id)].index.tolist()
         return random.choice(staff_ids)
 
-    def select_available_equipment_from_store(self, store_id: int, date: datetime.datetime, count=2) -> list:
-        # lended = inventory_id in lended inventory table where inspection_id and cancel_date are not defined
+    def select_inventory_from_store(self, store_id: int, count=2) -> list:
         inventory_of_store = self.__get_inventory_ids_for_store__(store_id)
-        #currently_lended_inventory = self.__get_overall_lended_inventory_ids__()
-        #available_inventory = list(set(inventory_of_store).difference(currently_lended_inventory))
-        return list(set(random.choices(inventory_of_store, k = count)))
+        return list(set(random.choices(inventory_of_store, k=count)))
 
     '''
         Defining Activities
@@ -114,27 +107,25 @@ class ProcessSimulator:
             self.lended_inventory.iloc[inventory_id, self.lended_inventory.columns.get_loc('lend_date')] = date
         print('Lended inventory ids ' + str(inventory_ids) + ' were picked up on ' + date.__str__())
 
-    def return_inventory(self, rental_id: int, inventory_ids: list, date: datetime.datetime):
+    def return_inventory(self, rental_id: int, lended_inventory_ids: list, date: datetime.datetime):
         additional_payment_ids = []
-        print('Lended inventory ids ' + str(inventory_ids) + ' were returned up on ' + date.__str__() + '.')
-        for inventory_id in inventory_ids:
-            inspection_id, payment_id = self.inspect_inventory(rental_id, inventory_id, date)
+        print('Lended inventory ids ' + str(lended_inventory_ids) + ' were returned up on ' + date.__str__() + '.')
+        for lended_inventory_id in lended_inventory_ids:
+            inspection_id, payment_id = self.inspect_inventory(rental_id, lended_inventory_id, date)
             if payment_id != -1:
                 additional_payment_ids.append(payment_id)
-            self.lended_inventory.iloc[inventory_id, self.lended_inventory.columns.get_loc('return_date')] = date
-            self.lended_inventory.iloc[inventory_id, self.lended_inventory.columns.get_loc('inspection_id')] = int(inspection_id)
+            self.lended_inventory.iloc[lended_inventory_id, self.lended_inventory.columns.get_loc('return_date')] = date
         return additional_payment_ids
 
-    def inspect_inventory(self, rental_id: int, inventory_id: int, date: datetime.datetime):
-        inspector_id = self.__get_staff_id_for_inspection__(inventory_id)
+    def inspect_inventory(self, rental_id: int, lended_inventory_id: int, date: datetime.datetime):
+        inspector_id = self.__get_staff_id_for_inspection__(lended_inventory_id)
+        new_inspection = {'inspector_id': inspector_id, 'inspection_date': date, 'lended_inventory_id': lended_inventory_id}
         payment_id = -1
         if random.uniform(0, 1) < 0.05:
-            payment_id = self.create_payment(rental_id, [inventory_id], date)
-            new_inspection = {'inspector_id': inspector_id, 'inspection_date': date, 'payment_id': payment_id}
-            print('Lended inventory id ' + str(inventory_id) + ' was inspected up on ' + date.__str__() + '. Due to damage of the equipment an additional payment was created.')
+            payment_id = self.create_payment(rental_id, [lended_inventory_id], date)
+            print('Lended inventory id ' + str(lended_inventory_id) + ' was inspected up on ' + date.__str__() + '. Due to damage of the equipment an additional payment with id' + str(payment_id) + 'was created.')
         else:
-            new_inspection = {'inspector_id': inspector_id, 'inspection_date': date}
-            print('Lended inventory id ' + str(inventory_id) + ' was inspected up on ' + date.__str__() + '.')
+            print('Lended inventory id ' + str(lended_inventory_id) + ' was inspected up on ' + date.__str__() + '.')
         self.inspections = self.inspections.append(new_inspection, ignore_index=True)
         inspection_id = self.inspections.index.max()
         return inspection_id, payment_id
@@ -147,7 +138,7 @@ class ProcessSimulator:
             current_time_process = current_time
 
             belonging_store_id = self.__get_store_of_customer__(c)
-            selected_inventory = self.select_available_equipment_from_store(belonging_store_id, current_time_process)
+            selected_inventory = self.select_inventory_from_store(belonging_store_id)
 
             rental_id, lended_inventory_ids = self.create_rental(c, selected_inventory, current_time_process)
 
@@ -180,17 +171,13 @@ class ProcessSimulator:
                     self.confirm_payment(additional_payment_id, current_time_process)
 
     def save_table_to_csv(self):
-        path = 'generatedData/'
-
-        self.rental_orders.to_csv(path + 'rental_orders.csv')
-        self.lended_inventory.to_csv(path + 'lended_inventory.csv')
-        self.inspections.to_csv(path + 'inspections.csv')
-        self.payments.to_csv(path + 'payments.csv')
+        self.rental_orders.to_csv(self.path + 'rental_orders.csv')
+        self.lended_inventory.to_csv(self.path + 'lended_inventory.csv')
+        self.inspections.to_csv(self.path + 'inspections.csv')
+        self.payments.to_csv(self.path + 'payments.csv')
 
 
 if __name__ == '__main__':
-
     ps = ProcessSimulator()
     ps.simulate_process(datetime.datetime(year=2020, month=1, day=1))
-
     ps.save_table_to_csv()
